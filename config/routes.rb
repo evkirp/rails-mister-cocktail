36039305d1855ee5d5eb325392fe3f69983a459d
Evgeniya Kirpicheva <jenny_blz@yahoo.com>
2018-11-15 14:15:56 +0100

    fixed cocktails model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'cocktails', to: 'cocktails#index'
+  get 'cocktails/new', to: 'cocktails#new'
+  post 'cocktails', to: 'cocktails#create'
+  get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cocktails#index'
 end
